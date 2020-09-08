81d55c5d91d7d084fbfc6fe1f32165a7159bfd3d
RLRabinowitz <30493345+RLRabinowitz@users.noreply.github.com>
2020-09-08 19:28:20 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,17 +9,6 @@ module "my_website" {
   html_file_source = "index.html"
 }
 
-module "my_new_website" {
-  source = "./my-website-bucket"
-
-  name = "env0.my-new-website.cd.blog.com"
-  html_file_source = "new.index.html"
-}
-
 output "my_website_endpoint" {
   value = module.my_website.website_endpoint
-}
-
-output "my_new_website_endpoint" {
-  value = module.my_new_website.website_endpoint
 }
